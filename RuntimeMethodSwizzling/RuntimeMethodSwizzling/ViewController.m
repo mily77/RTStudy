edbe9b4c732de0bac90079c61e8bb6e1c166d783
mily77 <773258709@qq.com>
2018-08-31 16:28:50 +0800

    init

---
+++ RuntimeMethodSwizzling/RuntimeMethodSwizzling/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RuntimeMethodSwizzling
+//
+//  Created by emily on 2018/8/31.
+//  Copyright © 2018年 emily. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ RuntimeMethodSwizzling/RuntimeMethodSwizzling/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RuntimeMethodSwizzling
+//
+//  Created by emily on 2018/8/31.
+//  Copyright © 2018年 emily. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
